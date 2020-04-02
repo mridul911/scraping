9e81294e8198e7b6d3ffbab544961d2ec88150b3
mridul911 <60569576+mridul911@users.noreply.github.com>
2020-04-02 23:12:59 +0530

    Update secondpart.py

--- pclub/secondpart.py
+++ pclub/secondpart.py
@@ -2,14 +2,14 @@ import csv
 import json
 import re
 import pandas as pd
-def white(s):
+def white(s):  #calculating if it has single or multiple words
     count=0
     for a in s:
         if(a.isspace())== True:
             count+=1
     return count
-string_check= re.compile('[@_!#$%^&*()<>?/\|}{~:]')
-with open('GSOC 2019.csv', 'r',encoding="utf8") as f:
+string_check= re.compile('[@_!#$%^&*()<>?/\|}{~:]') #check if it has any special character or not
+with open('GSOC 2019.csv', 'r',encoding="utf8") as f:#opening file and reading it
     d_reader = csv.DictReader(f)
     headers = d_reader.fieldnames
     wname=[]
@@ -21,7 +21,7 @@ with open('GSOC 2019.csv', 'r',encoding="utf8") as f:
         else:
             wname.append(line['name'])
 print(wname)
-#pd.read_json('students.json').to_csv('students.csv')
+#pd.read_json('students.json').to_csv('students.csv') converting json to csv type
 with open('students.csv','r',encoding="utf8") as f1:
     d_reader1 = csv.DictReader(f1)
     headers1= d_reader1.fieldnames
